0990e9afd90f3d5c86d3c844c04504802464856b
adfirliyadi <firliyadi@gmail.com>
2021-09-25 23:14:23 +0700

    menambah dependency
    add dependency in main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adfirliyadi/app-say-hello.git
 
 go 1.15
+
+require github.com/adfirliyadi/go-say-hello2 v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/adfirliyadi/go-say-hello2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
